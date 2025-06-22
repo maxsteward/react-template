a06a2332d7ec07b34c07f61756ba4e5024788c27
Max Steward <maxwell.steward@g.austincc.edu>
2025-06-21 20:26:58 -0500

    send it

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is an h1 heading!</h1>
+        <p>
+          This is a paragraph.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
